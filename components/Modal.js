2fc4b44001f88752d6fb7189ce101c272e1be131
Furrukh Jamal <furrukhjamal@yahoo.com>
2022-03-20 16:25:37 +0500

    modal not closing

--- components/Modal.js
+++ components/Modal.js
@@ -1,19 +1,35 @@
 import { StyleSheet, Text, View, Modal, Pressable, Image } from 'react-native'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import AppContext from '../context/appContext'
 import useFetch from '../hook/useFetch'
 
 export default function UserInfoModal(props) {
-    //const{data : followers, isFollowersLoading} = useFetch(props.data.followers_url)
-    //const{data : following, isFollowingLoading} = useFetch(props.data.following_url)
     
-    const{data : followers, isFollowersLoading} = useFetch(`https://api.github.com/users/${props.data.login}/followers`)
-    const{data : following, isFollowingLoading} = useFetch(`https://api.github.com/users/${props.data.login}/following`)
+    const[followers, setFollowers] = useState(0)
+    const[following, setFollowing] = useState(0)
+    const[name, setName] = useState("")
+    const[location, setLocation] = useState(null)
     const{setModalVisible, modalVisible} = useContext(AppContext)
+
+    useEffect(()=>{
+      async function getUserData(){
+        let response = await fetch(`https://api.github.com/users/${props.data.login}`)
+        let result = await response.json()
+        return result
+
+      }
+      (async ()=>{
+        let result = await getUserData()
+        setFollowers(result.followers)
+        setFollowing(result.following)
+        setName(result.name)
+        setLocation(result.location)
+      })()
+    },[])
         return (
           
           <Modal
-            animationType="none"
+            animationType="slide"
             transparent={true}
             visible={modalVisible}
             
@@ -26,9 +42,22 @@ export default function UserInfoModal(props) {
                   source = {{uri : props.data.avatar_url}}
                 />
                 
-                <Text style={styles.modalText}>Hello World!</Text>
+                <Text style={[styles.modalText, {fontWeight : "bold"}]}>{name}</Text>
+
+                {location && <Text>{location}</Text>}
+                
+                <View style = {styles.modalRow}>
+                  <Text style = {{fontWeight : "bold"}}>Followers : </Text>
+                  <Text>{followers}</Text>
+                </View>
+
+                <View style = {styles.modalRow}>
+                  <Text style = {{fontWeight : "bold"}}>Following : </Text>
+                  <Text>{following}</Text>
+                </View>                
+                
                 <Pressable
-                  style={[styles.button, styles.buttonClose]}
+                  style={[styles.button, styles.buttonClose, {marginTop : 10}]}
                   onPress={() => setModalVisible(!modalVisible)}
                 >
                   <Text style={styles.textStyle}>Close</Text>
@@ -79,6 +108,9 @@ const styles = StyleSheet.create({
   buttonClose: {
     backgroundColor: "#2196F3",
   },
+  modalRow : {
+    flexDirection : "row"
+  }
 })
 
 
--- hook/useFetch.js
+++ hook/useFetch.js
@@ -12,10 +12,26 @@ export default function useFetch(url) {
             //console.log("Custom hoook async function working")
             try
             {
-                let response = await fetch(url)
-                let result = await response.json()
-               // console.log("Result in custom hook", result)
-                return result.length        
+                
+
+                //looping over all the pages for the list of followers/followings
+                let pageNumber = 1
+                let follower_following = []
+                do
+                {
+                    let urlWithParams = url + "?per_page=100&page=" + pageNumber    
+                    console.log(urlWithParams)
+                    let response = await fetch(`${urlWithParams}`)
+                    let result = await response.json()
+                    //console.log("RESULT IN CUSTOM", result)
+                    follower_following = [...follower_following , result]
+                    pageNumber++
+                }
+                while(pageNumber <=1)
+                // let response = await fetch(url)
+                // let result = await response.json()
+                console.log("Result in custom hook", follower_following.length)
+                return follower_following.length        
             } 
             catch (error)
             {
